e95186ce2b1f95fd97dcc57a539ed707523bd810
2heunxun <seapeon@naver.com>
2025-05-14 23:18:01 +0900

    [docs] User 클래스 작성 이유 및 이해를 위한 주석 추가

--- src/main/java/com/kvote/backend/domain/User.java
+++ src/main/java/com/kvote/backend/domain/User.java
@@ -29,3 +29,16 @@ public class User {
         this.refreshToken = refreshToken;
     }
 }
+/*
+AccessToken = 로그인 후 API 요청할 때 사용하는 짧은 유효기간을 가진 토큰임
+refreshToken = accessToken 만료 됐을때 새로 발급 받아야함
+
+AccessToken < refreshToken ( 기간으로 따졌을 때 )  ㅇㅇ
+
+accessToken은 유효시간 짧음 -> 서버에 저장 안 해도 됨 JWT니깐 유저정보 자체를 담고있어서 검증만 하면 됨
+refreshToken => 탈취 위험이 큼 -> 로그아웃 하면 폐기 처분 ? 같은 블랙리스트 처리 해줘야함
+Redis나 DB에 저장해서 관리 요망
+
+흐음 -> 쉽게 말하면
+accessToken은 사용자가 들고 댕기고 refreshToken은 서버가 검증용으로 저장함->그래서 User클래스에 refreshToken만 있는겁니당
+ */
