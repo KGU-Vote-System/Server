f64e8ca98b605a830d6540a3e93f8c2472ab83ac
kyu-0907 <123appp@kyonggi.ac.kr>
2025-06-02 18:03:56 +0900

    refactor: 스웨거 어노테이션 추가

--- src/main/java/com/kvote/backend/controller/NoticeController.java
+++ src/main/java/com/kvote/backend/controller/NoticeController.java
@@ -5,6 +5,7 @@ import com.kvote.backend.domain.Campus;
 import com.kvote.backend.dto.NoticeRequestDto;
 import com.kvote.backend.dto.NoticeResponseDto;
 import com.kvote.backend.service.NoticeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,7 @@ public class NoticeController {
 
     // 공지사항 생성
     @PostMapping
+    @Operation(summary = "create a new Notice")
     public ResponseEntity<NoticeResponseDto> createNotice(@Valid @RequestBody NoticeRequestDto dto) {
         NoticeResponseDto response = noticeService.creatNotice(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -29,13 +31,15 @@ public class NoticeController {
 
     // 단일 공지사항 조회
     @GetMapping("/{id}")
+    @Operation(summary = "read a single notice")
     public ResponseEntity<NoticeResponseDto> getNotice(@PathVariable Long id) {
         NoticeResponseDto response = noticeService.getNotice(id);
         return ResponseEntity.ok(response);
     }
 
-    // 캠퍼스별 모든 공지사항 조회 (최신순)
+
     @GetMapping("/notices/campus/{campus}")
+    @Operation(summary = "read notices by campus ")
     public ResponseEntity<List<NoticeResponseDto>> getNoticesByCampus(@PathVariable Campus campus) {
         List<NoticeResponseDto> notices = noticeService.getNoticesByCampus(campus);
         return ResponseEntity.ok(notices);
@@ -44,6 +48,7 @@ public class NoticeController {
 
     // 공지사항 수정
     @PutMapping("/{id}")
+    @Operation(summary = "update a notice")
     public ResponseEntity<NoticeResponseDto> updateNotice(@PathVariable Long id,
                                                           @Valid @RequestBody NoticeRequestDto dto) {
         NoticeResponseDto response = noticeService.updateNotice(id, dto);
@@ -52,6 +57,7 @@ public class NoticeController {
 
     // 공지사항 삭제
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete a notice")
     public ResponseEntity<Void> deleteNotice(@PathVariable Long id) {
         noticeService.deleteNotice(id);
         return ResponseEntity.noContent().build();
