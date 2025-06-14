cbb034cc7e0b9049767cde4d8fbed1f66703c727
iamjunhyeong <dddodo447@gmail.com>
2025-06-14 18:56:00 +0900

    Refact: @Schema 추가 - Election dto 부분 - Candidate dto 부분

--- src/main/java/com/kvote/backend/dto/CandidateRequestDto.java
+++ src/main/java/com/kvote/backend/dto/CandidateRequestDto.java
@@ -1,5 +1,6 @@
 package com.kvote.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -7,7 +8,10 @@ import lombok.*;
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter
 public class CandidateRequestDto {
+    @Schema(description = "후보자 이름", example = "홍길동")
     private String name;  // 후보자 이름
-    private String description;  // 후보자 설명
+    @Schema(description = "후보자 설명", example = "이 후보자는 훌륭한 리더입니다.")
+    private String description;  // 후보자 설명")
+    @Schema(description = "소속 선거 ID", example = "1")
     private Long electionId;  // 소속 선거 ID
 }
--- src/main/java/com/kvote/backend/dto/ElectionRequestDto.java
+++ src/main/java/com/kvote/backend/dto/ElectionRequestDto.java
@@ -3,6 +3,7 @@ package com.kvote.backend.dto;
 import com.kvote.backend.domain.Campus;
 import com.kvote.backend.domain.Election;
 import com.kvote.backend.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -15,10 +16,15 @@ import java.util.Date;
 @Getter @Setter
 @Builder
 public class ElectionRequestDto {
+    @Schema(description = "선거 제목", example = "2023년도 총학생회 선거")
     private String title;
+    @Schema(description = "선거 설명", example = "2023년도 총학생회 선거에 참여해주세요!")
     private String description;     // 선거 설명
+    @Schema(description = "투표 시작 시각", example = "2023-10-01T00:00:00Z")
     private Date startAt;  // 투표 시작 시각
+    @Schema(description = "투표 마감 시각", example = "2023-10-01T23:59:59Z")
     private Date endAt;    // 투표 마감 시각
+    @Schema(description = "캠퍼스", example = "SEOUL or SUWON or ALL")
     private Campus campus;
 
     public Election toEntity(BigInteger electionId, String txHash, User owner) {
